ac0e0062e3b83015ddb1afe00af55bf5917157ce
Sravya <ksravya539@gmail.com>
2020-04-14 19:03:57 -0700

    second

---
+++ Community/Community/obj/Debug/90/android/src/md5f8111d1f1253595edc2c6e01104bff10/MainActivity.java
@@ -0,0 +1,56 @@
+package md5f8111d1f1253595edc2c6e01104bff10;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"";
+		mono.android.Runtime.register ("Community.MainActivity, Community", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Community.MainActivity, Community", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
